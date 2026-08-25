cd624b38cad200923c755b62b42063c2a1b076a1
242031009-torikul <242031009@feniuniversity.ac.bd>
2026-08-25 06:52:31 +0600

    Update Bfs.py

--- Bfs.py
+++ Bfs.py
@@ -1,1 +1,31 @@
+from collections import deque
+
+tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': [],
+    'E': [],
+    'F': [],
+    'G': []
+}
+
+def bfs(tree, start):
+    order=[]
+    queue = deque([start])
+    while queue:
+        order.append(queue.popleft())
+        node = order[-1]
+      
+        for child in tree[node]:
+            queue.append(child)
+
+    return order
+
+
+start = 'A'
+
+result = bfs(tree, start)
+
+print(result)
 
